661a74231bc9f1aa4ce4882d083fb4767f3e2313
unikdahal <61407386+Unik-Dahal@users.noreply.github.com>
2022-04-30 10:18:44 +0530

    Update question1.c

--- 30th April/question1.c
+++ 30th April/question1.c
@@ -8,10 +8,12 @@ int main(){
 	}
 			
 	for(int i=0;i<3;i++){
-			for(int j=0;j<=i;j++){
-					printf("%d",arr[i][j]);
+		
+			for(int j=0;j<i;j++){
+				printf("  ");
+			}
+			for(int j=i;j<3;j++){
+					printf(" %d",arr[i][j]);
 			}
 			printf("\n");
 	}
-	
-}
