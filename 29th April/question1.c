da7d05aef5f6e98c1feff11383000b9872554c80
unikdahal <61407386+Unik-Dahal@users.noreply.github.com>
2022-04-30 10:10:33 +0530

    Add files via upload

---
+++ 29th April/question1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int arr[3][3];
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++)
+			scanf("%d",&arr[i][j]);
+		
+	}
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++)
+			printf("%d",arr[i][j]);
+		printf("\n");
+	}
+}
