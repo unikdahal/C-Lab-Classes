591eb50f0a96b8d568fc1aa63643f23ef24d3614
Unik-Dahal <unikdahal03@gmail.com>
2022-05-04 11:04:57 +0530

    Question4.c

---
+++ 4th May/question4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+    int a=5;
+    int b=6;
+    int c=7;
+
+    int *aptr=&a;
+    int *bptr=&b;
+    int *cptr=&c;
+
+    if(aptr<bptr){
+        if(aptr<cptr){
+            printf("%d\n",*aptr);
+        }
+        else{
+            printf("%d\n",*cptr);
+        }
+    }
+    else{
+        if(bptr<cptr){
+            printf("%d\n",*bptr);
+        }
+        else{
+            printf("%d\n",*cptr);
+        }
+    }
+    return 0;
+}
