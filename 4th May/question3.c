1b8c1f4d4d4b6da942a16bf6b5576d83c4c80f74
Unik-Dahal <unikdahal03@gmail.com>
2022-05-04 10:59:57 +0530

    Question3.c

---
+++ 4th May/question3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+    int a=5;
+    int b=6;
+
+    int *aptr=&a;
+    int *bptr=&b;
+
+    *aptr=a+b;
+    *bptr=*aptr-b;
+    *aptr=*aptr-*bptr;
+
+    printf("The value of a is %d \n",*aptr);
+    printf("The value of b is %d",*bptr);
+
+    return 0;
+}
