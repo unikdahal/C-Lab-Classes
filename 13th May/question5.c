111f47a17ceaf7456a340f05032520395fa4206c
Unik-Dahal <unikdahal03@gmail.com>
2022-05-13 10:17:28 +0530

    13th May

--- 13th May/question5.c
+++ 13th May/question5.c
@@ -1,9 +1,21 @@
 //WAP TO DIFFERENTIATE CALL BY VALUE AND CALL BY REFERENCE
 
 #include<stdio.h>
+
+int f1(int a){
+    a++;
+}
+int f2(int **b){
+    (**b)++;
+}
+
+
 int main(){
     int a=5;
-    int b=a;
-    printf("%d\n",b);
+    int *b=&a;
+    f1(a);
+    printf("%d\n",a);
+    f2(&b);
+    printf("%d\n",a);
     return 0;
 }
