af6b923012ba0edf64973ffa838d3404b5febef9
Unik-Dahal <unikdahal03@gmail.com>
2022-05-16 20:18:47 +0530

    Updated

--- 11th  May/question8.c
+++ 11th  May/question8.c
@@ -1,2 +1,22 @@
 //WAP to display fibonacci series upto nth position
 
+#include<stdio.h>
+
+int f1(int n){
+    int a=1,b=1,i=1;
+    printf("%d %d ",a,b);
+    while (i<n-1)
+    {
+        int c=a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+        i++;
+    }
+    return 0;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    f1(n);
+}
