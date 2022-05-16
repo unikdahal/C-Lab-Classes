5c1c288470497e2a34b6df70e4fbdd7170d793b8
Unik-Dahal <unikdahal03@gmail.com>
2022-05-16 20:11:19 +0530

    Updated

--- 11th  May/question6.c
+++ 11th  May/question6.c
@@ -1,5 +1,20 @@
 //Take a string and display in the following format 
 
 // AbcD PklM nT  VPR
-// Abcd Pklm Nt Vpr
-//A. P. N. Vpr
+//A. P. N. Vpr]
+
+#include<stdio.h>
+int main(){
+    char a[50];
+    scanf("%[^\n]s",a);
+    int i,j,k=0;
+    a[0]=a[0]-32;
+    printf("%c.",a[0]);
+    for(i=1;a[i]!='\0';i++)
+    {
+        if(a[i]==' ')
+        {
+            printf(" %c.",a[i+1]-32);
+        }
+    }
+}
