89becdd24fadeaa59cfc5410f4ac8cf9e496a8cf
Unik-Dahal <unikdahal03@gmail.com>
2022-05-18 12:52:49 +0530

    18th May

--- 18th May/question5.c
+++ 18th May/question5.c
@@ -1,15 +1,1 @@
-//Reverse a number using recursive function
-#include<stdio.h>
-void dis(int n){;
-    if(n>0)
-    {
-        printf("%d",n%10);
-        dis(n/10);   
-    }
-}
-    int main(){
-        int n;
-        scanf("%d",&n);
-        dis(n);
-        return 0;
-}
+
